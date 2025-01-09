ba742ef826eca0648922c88f69447317cfb7e955
baierma <baierm@gmx.net>
2025-01-09 10:23:00 +0100

    add to git

--- main.py
+++ main.py
@@ -24,3 +24,32 @@ while i <= 100:
     else:
         print(i)
     i = i + 1
+
+
+m = 1
+while m <= 100:
+    if m % 5 == 0:
+        print("Digital")
+    else:
+        print(m)
+    m = m + 1
+
+
+#Aufgabe 4
+#ersetze alle Zahlen, die durch drei Teilbar sind mit String "digital" und durch 5 mit "history"
+i = 1
+while i <= 100:
+    if i % 3 == 0 and i % 5 == 0: #oder einmal durch 15
+        print("digital history")
+    elif i % 3 == 0:
+        print("Digital")
+    elif i % 5 == 0:
+        print("History")
+    else:
+        print(i)
+    i = i + 1
+
+
+
+#Aufgabe 5
+
